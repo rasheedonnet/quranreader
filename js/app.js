c1fb41cf4a11e6b33821d7f840ad48e7d69fb700
Abdul Rasheed Nagoor Gani <abdulrasheednagoorgani@Jayalakshmis-MacBook-Pro.local>
2021-01-01 19:52:27 -0500

    improved stop and reset

--- js/app.js
+++ js/app.js
@@ -8,46 +8,60 @@ window.onload = function(){
     let recitation = $('#list-recitation');
     let translation = $('#list-translation');
     let playProgress = $('#playProgress');
+    let playBtn = $('#playBtn');
+    var myAudio = document.getElementById('player');
+    var playlist = new Array();
 
     initializePage();
 
     playBtnClick = function() { 
-        var myAudio = document.createElement('audio');
         var audioSourceBaseUrl = 'https://everyayah.com/data/';
 
-        $('#playBtn').prop('disabled', true);
-        // audioSourceBaseUrl = audioSourceBaseUrl + recitation.val() + "/";
-        var playlist = createPlaylist();
-        i = 0;
-        
-        myAudio.addEventListener('ended', function () {
-            i = ++i;
-            
-            if(i == playlist.length){
-                myAudio.pause();
-                $('#playBtn').prop('disabled', false);
-                playProgress.text("");
-                return;
+        if (playBtn.text() == 'Play'){
+            playBtn.text('Stop');
+            if(myAudio.paused && playlist.length > 0){
+                myAudio.play();
             }
             else{
-                playProgress.text("Playing ayah:" + playlist[i].split('/').pop().substring(3,6));
+                playlist = createPlaylist();
+                i = 0;
+                myAudio.addEventListener('ended', function () {
+                    i = ++i;
+                    
+                    if(i == playlist.length){
+                        myAudio.pause();
+                        playBtn.text('Play');
+                        playProgress.text("");
+                        return;
+                    }
+                    else{
+                        playProgress.text("Playing ayah:" + playlist[i].split('/').pop().substring(3,6));
+                    }
+                    myAudio.src = audioSourceBaseUrl + playlist[i];
+                    
+                    myAudio.play();
+                }, true);
+                myAudio.loop = false;
+                myAudio.src = audioSourceBaseUrl + playlist[0];
+                myAudio.play();
             }
-            
-            myAudio.src = audioSourceBaseUrl + playlist[i];
-            
-            myAudio.play();
-        }, true);
-        myAudio.loop = false;
-        myAudio.src = audioSourceBaseUrl + playlist[0];
-        myAudio.play();
+        }
+        else{
+            playBtn.text('Play');  
+            myAudio.pause();
+        }
     }
     
-    function initializePage()
-    {
+    resetBtnClick = function() { 
+        location.reload();
+    }
+
+    function initializePage(){
         surahSelect.empty();
         surahSelect.append('<option selected="true" disabled>Select Surah</option>');
         surahSelect.prop('selectedIndex', 0);
-
+        ayahStart.empty();
+        ayahEnd.empty();
         // Populate dropdown with list of suras
         $.getJSON(surahJson, function (data) {
             $.each(data.quran.suras.sura, function (key, entry) {
@@ -86,14 +100,14 @@ window.onload = function(){
         if(surahSelect.val()!="1"){
             ayas.push(recitation.val() + "/" + bismillah)
         }
-        for (i=parseInt(ayahStart.val());i<=parseInt(ayahEnd.val());i++){
+        for (i=parseInt(ayahStart.val()); i<=parseInt(ayahEnd.val()); i++){
             ayas.push(recitation.val() + "/" + prependZero(surahSelect.val()) + prependZero(i) + '.mp3')
             ayas.push(translation.val() + "/" + prependZero(surahSelect.val()) + prependZero(i) + '.mp3')
         }
         
-        var playList = niyath.concat(ayas);
-        return playList;
+        return niyath.concat(ayas);
     }
+
     function prependZero(number) {
         if (number <= 9){
             return "00" + number;
